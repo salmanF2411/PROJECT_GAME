2f126afc3cd303ea7a7aa8e57f8f343187bf89ff
salmanF2411 <salman241105@email.com>
2026-03-18 12:14:23 +0700

    Add Menu Manager script

---
+++ Assets/Script/Menu_Manager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Menu_Manager : MonoBehaviour
+{
+
+    public TMP_Text panelName;
+    public void LoadScene(string sceneName)
+    {
+       SceneManager.LoadScene(sceneName); 
+    }
+
+    public void ExitApp ()
+    {
+        Application.Quit();
+    }
+
+    public void setPanelName(string name)
+    {
+        panelName.text = name;
+    }
+}
